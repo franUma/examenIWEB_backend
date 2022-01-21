a028bbf219a0b92ee1d2c2b943d3ac0fef3d8a49
franUma <fran30032000@uma.es>
2022-01-21 19:22:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from google.auth.transport import requests
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['MONGO_URI']='mongodb+srv://pass123:pass123@cluster0.hmc2h.mongodb.net/examenWEB?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE'
@@ -16,7 +17,8 @@ CORS(app)
     # Creamos la conexión
 mongo = PyMongo(app)
 usuarios = mongo.db.usuarios
-photoNet = mongo.db.photoNet
+articulos = mongo.db.articulos
+pujas = mongo.db.pujas
 
 cloudinary.config( 
   cloud_name = "webfgp", 
@@ -58,70 +60,70 @@ def subirFoto():
     res = cloudinary.uploader.upload(foto)
     url = res["url"]
     return jsonify({"url":url})
+#################################################################################################################################
 
-
-@app.route('/subirFotoBD', methods=['POST'])
-def subirFotoBD():
+@app.route('/articulos', methods=['POST'])
+def añadirArticulo():
     datos = request.get_json()
     token = request.headers["Authorization"].split(' ')[1]
     if (id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)):
-        try:
-            foto = photoNet.find_one({"email": str(datos["email"]), 
-                                "url": str(datos["url"])})
-            if foto is None:
-                res = photoNet.insert_one({
-                    "email": str(datos["email"]),
-                    "descripcion": str(datos["descripcion"]),
-                    "url": str(datos["url"]),
-                    "likes": 0
-                })
-                return jsonify(msg="Post creado", new_id=str(res.inserted_id))
-            else:
-                return jsonify(msg="Foto ya subida")
-        except:
-            respuesta = jsonify(msg="Petición no válida, faltan campos o no son del tipo correcto")
-            return respuesta
+        idArticulo = articulos.insert_one({
+            "vendedor": str(datos["vendedor"]),
+            "descripcion": str(datos["descripcion"]),
+            "precio_salida": datos["precio_salida"],
+            "imagenes": str(datos["imagenes"]),
+            "comprador": "null"
+        })
+        return jsonify(msg="Articulo añadido", new_id=str(idArticulo.inserted_id))
     else:
         return jsonify(msg="Token no válido")
     
     
-@app.route('/fotos', methods=['GET'])
-def getFotos():
+@app.route('/articulos', methods=['GET'])
+def getArticulos():
     resultado = []
-    for d in photoNet.find().sort("likes", -1):
+    for d in articulos.find():
         d["_id"] = str(d["_id"])
+        # Puja mayor
+        res = 0
+        for p in pujas.find({"identificador": d["_id"]}).sort("cantidad_ofrecida", -1):
+            res = p["cantidad_ofrecida"]
+            break
+        d["pujaMayor"] = str(res)
         resultado.append(d)
     return jsonify(resultado)
+    
 
-
-@app.route('/like/<id>', methods=['PUT'])
-def darLike(id):
+@app.route('/pujar', methods=['POST'])
+def pujar():
+    datos = request.get_json()
     token = request.headers["Authorization"].split(' ')[1]
     if (id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)):
-        oid = ObjectId(id)
-        resultado = photoNet.find_one({"_id" : oid})
-        if resultado is not None:
-            if request.is_json:
-                datos = request.get_json()
-                nuevos_valores = {}
-                try:
-                    if "likes" in datos:
-                        nuevos_valores["likes"] = int(datos["likes"])
-                except Exception:
-                    respuesta = jsonify(msg="Petición no válida, hay campos que no son del tipo correcto")
-                    return respuesta
-
-                photoNet.update_one({"_id": oid}, {"$set": nuevos_valores})
-                return jsonify(msg='Usuario actualizado')
-            else:
-                respuesta = jsonify(msg="Petición no válida, se requiere JSON")
-                return respuesta
-        else:
-            respuesta = jsonify(msg="No existe ningún usuario con id = %s" % id)
-            return respuesta
+        # Añadir nueva puja
+        now = datetime.now()
+        idPuja = pujas.insert_one({
+            "identificador": str(ObjectId(datos["identificador"])),
+            "comprador": str(datos["comprador"]),
+            "timestamp": str(now.strftime("%d/%m/%Y %H:%M")),
+            "cantidad_ofrecida": datos["cantidad_ofrecida"],
+        })
+        return jsonify(msg="Puja añadida", new_id=str(idPuja.inserted_id))
     else:
         return jsonify(msg="Token no válido")
 
+
+@app.route('/pujaMasAlta/<id>', methods=['GET'])
+def getPujaMayor(id):
+    token = request.headers["Authorization"].split(' ')[1]
+    if (id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)):
+        res = "None"
+        for p in pujas.find({"identificador": id}).sort("cantidad_ofrecida", -1):
+            res = p["cantidad_ofrecida"]
+            break
+        return jsonify(msg=res)
+    else:
+        return jsonify(msg="Token no válido")
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
